0a6c9ffa8c733098707dbcbe1f28509ebf8de9b4
Miriam Castillo <castillo.m.mc@gmail.com>
2017-12-10 02:32:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function logDriverNames(drivers) {
-drivers.forEach(function(driver) {
+  drivers.forEach(function(driver) {
   console.log(driver.name)
+})
+}
+
+function logDriversByHometown(drivers) {
+   drivers.forEach(function(driver) {
+   console.log(driver.hometown)
 })
 }
