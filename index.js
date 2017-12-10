da813e505edc1ae0567fe7c63ac87a7bf8ee2b25
Miriam Castillo <castillo.m.mc@gmail.com>
2017-12-10 02:20:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-logDriverNames(drivers){
-  const drivers = [
-    { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
-    { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
-    { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
-    { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
-    { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
-  ];
+drivers.forEach(logDriverNames(){
+  console.log(drivers);
 }
+});
